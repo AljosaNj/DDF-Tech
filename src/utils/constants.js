f15266b6f133a995c607b1ecfbf285c285f741fd
AljosaNj <aljosaa.njezic@gmail.com>
2021-09-23 18:26:43 +0200

    first commit

---
+++ src/utils/constants.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { FaDollarSign, FaRedo, FaTruckMoving } from 'react-icons/fa'
+export const links = [
+  {
+    id: 1,
+    text: 'home',
+    url: '/',
+  },
+  {
+    id: 2,
+    text: 'about',
+    url: '/about',
+  },
+  {
+    id: 3,
+    text: 'products',
+    url: '/products',
+  },
+]
+
+export const services = [
+  {
+    id: 1,
+    icon: <FaDollarSign />,
+    title: 'Secured Payment',
+    text:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi',
+  },
+  {
+    id: 2,
+    icon: <FaRedo />,
+    title: 'Free Return',
+    text:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi',
+  },
+  {
+    id: 3,
+    icon: <FaTruckMoving/>,
+    title: 'Express Delivery',
+    text:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi',
+  },
+]
+
+export const products_url = 'https://serv-react-api.netlify.app/api/3-z-complete'
+
+export const single_product_url = `https://serv-react-api.netlify.app/api/3-z-complete?id=`
